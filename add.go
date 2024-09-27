03b5f1d82875e6a703f16502cd2549f9047ad7e7
dagash007 <balogunabdulqudusolakunle@gmail.comllllhhhbbbhhshhgggvvvvvkkjlk>
2024-09-27 17:04:53 -0400

    Dagash

---
+++ add.go
@@ -0,0 +1,6 @@
+package main
+
+// Function to add two numbers
+func addNumbers(a int, b int) int {
+    return a + b
+}
--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Welcome to Group E's Week 4 Project!")
+    fmt.Println("Welcome to Group A's Week 4 Project!")
+
+    // Example inputs
+    num1 := 5
+    num2 := 10
+    sum := addNumbers(num1, num2)
+
+    fmt.Printf("The sum of %d and %d is %d\n", num1, num2, sum)
 }
